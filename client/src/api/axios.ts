b1c1227a4f52891ee60aa070608278cff5a3858e
parvgothwal03 <parvgothwal0310@gmail.com>
2026-07-22 16:34:13 +0530

    Deployment

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000"
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://social-scheduler-backend-wl1i.onrender.com"
 });
 
 export default api;
